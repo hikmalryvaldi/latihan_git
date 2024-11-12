8ffa17ec74205c9e5427b78c705465f3effab1f7
Novan <novanramdan7@gmail.com>
2024-11-12 10:13:58 +0700

    latihan halamanAbout

---
+++ resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ini Halaman About</title>
+</head>
+
+<body>
+    <h1>Halaman About</h1>
+    <p>TES TES 123 INI NOVAN RAMDAN</p>
+    <p>Persib bandung</p>
+</body>
+
+</html>
